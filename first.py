4c952b7e07c779d781ebc17c817f10a37cc4c03e
saminaattaria <137102347+saminaattaria@users.noreply.github.com>
2024-10-30 17:45:00 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,40 @@
+# 01 Write a program that calculates the sum, difference, product, and average of three numbers:
+num1=10
+num2=20 
+num3=30
+print(num1+num2+num3)
+print (num1-num2-num3)
+print (num1*num2*num3)
+#average=total_sum/3
+print(60/3)# 20.0 ans
+
+# 02 Write Python code to calculate the area of the following shapes:
+#area of triangle
+area=0.5
+base=20 
+height=5
+print(0.5*20*5) # 50.0 ans
+
+#area of square
+area=side=5
+print(side*side) # 25 ans
+
+#area of circle
+area=pi=3.14
+radius=10
+print(3.14*radius*radius) # 314.0 ans
+
+#area of rectangle
+length=10
+width=15
+print(length*width) #150 ans
+
+    
+    
+    
+    
+   
+
+
+
+
